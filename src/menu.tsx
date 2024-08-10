efbcb58dc1a5e03fe6fba2cab8fd975479ad514e
Shahar Sonino <shaharso@microsoft.com>
2024-08-10 10:42:21 +0300

    add new wheels types

--- src/menu.tsx
+++ src/menu.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './Menu.css'; // Assuming you will create a CSS file for styling
 
 const menuItems = [
-  { name: 'גלגל שמות', image: 'https://th.bing.com/th/id/OIP.GPg0Sc8awrPLPPD4_7wJvQHaFj?rs=1&pid=ImgDetMain', url: 'https://www.bing.com/ck/a?!&&p=658073dd52b26c41JmltdHM9MTcyMzE2MTYwMCZpZ3VpZD0wMGE2NmFkYy1mMjFiLTY1ODAtMWVhMi03OWIzZjM4YjY0OTQmaW5zaWQ9NTE5NQ&ptn=3&ver=2&hsh=3&fclid=00a66adc-f21b-6580-1ea2-79b3f38b6494&psq=%d7%92%d7%9c%d7%92%d7%9c+%d7%a9%d7%9e%d7%95%d7%aa&u=a1aHR0cHM6Ly93aGVlbG9mbmFtZXMuY29tL2hlLw&ntb=1' },
+  { name: 'גלגל שמות', image: 'https://th.bing.com/th/id/OIP.GPg0Sc8awrPLPPD4_7wJvQHaFj?rs=1&pid=ImgDetMain', url: 'https://wheelofnames.com/he/n7s-4x8' },
+  { name: 'צבעים באנגלית', image: 'https://th.bing.com/th/id/OIP.GPg0Sc8awrPLPPD4_7wJvQHaFj?rs=1&pid=ImgDetMain', url: 'https://wheelofnames.com/he/822-us7' },
   { name: 'פקמן', image: 'https://th.bing.com/th/id/OIP.WnQgGM1kKQDCVouw8iO5kQHaEc?w=300&h=180&c=7&r=0&o=5&pid=1.7', url: 'https://pacman.js.org/' },
   { name: 'חשבון', image: 'https://th.bing.com/th/id/OIP.YQt1G5Ho2Gv6jzCcktHDpwHaHa?w=165&h=180&c=7&r=0&o=5&pid=1.7', url: 'https://www.matific.com/students/app/7.5.7/lite/' },
   // Add more items as needed

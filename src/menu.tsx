4ded058c480f897ffd82c33b833a77ad13dcec52
Shahar Sonino <shaharso@microsoft.com>
2024-08-10 21:51:00 +0300

    change the wheel images

--- src/menu.tsx
+++ src/menu.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Menu.css'; // Assuming you will create a CSS file for styling
+import WheelBodyPartsImage from './assets/wheel_body_parts.jpg';
+import WheelColorImage from './assets/wheel_colors.png';
+import WheelFamilyNames from './assets/wheel_family_names.png';
+
 
 const menuItems = [
-  { name: 'גלגל שמות', image: 'https://th.bing.com/th/id/OIP.GPg0Sc8awrPLPPD4_7wJvQHaFj?rs=1&pid=ImgDetMain', url: 'https://wheelofnames.com/he/n7s-4x8' },
-  { name: 'צבעים באנגלית', image: 'https://th.bing.com/th/id/OIP.GPg0Sc8awrPLPPD4_7wJvQHaFj?rs=1&pid=ImgDetMain', url: 'https://wheelofnames.com/he/822-us7' },
-  { name: 'חלקי גוף', image: 'https://th.bing.com/th/id/OIP.GPg0Sc8awrPLPPD4_7wJvQHaFj?rs=1&pid=ImgDetMain', url: 'https://wheelofnames.com/he/5f6-3pn' },
+  { name: 'גלגל שמות', image: WheelFamilyNames, url: 'https://wheelofnames.com/he/n7s-4x8' },
+  { name: 'צבעים באנגלית', image: WheelColorImage, url: 'https://wheelofnames.com/he/822-us7' },
+  { name: 'חלקי גוף', image: WheelBodyPartsImage, url: 'https://wheelofnames.com/he/5f6-3pn' },
   { name: 'פקמן', image: 'https://th.bing.com/th/id/OIP.WnQgGM1kKQDCVouw8iO5kQHaEc?w=300&h=180&c=7&r=0&o=5&pid=1.7', url: 'https://pacman.js.org/' },
   { name: 'חשבון', image: 'https://th.bing.com/th/id/OIP.YQt1G5Ho2Gv6jzCcktHDpwHaHa?w=165&h=180&c=7&r=0&o=5&pid=1.7', url: 'https://www.matific.com/students/app/7.5.7/lite/' },
   // Add more items as needed

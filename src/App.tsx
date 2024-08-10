97ef791fa9595c546245fc0186710d93df7cf0d8
Shahar Sonino <shaharso@microsoft.com>
2024-08-10 23:30:15 +0300

    add all brawls stars

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,33 @@ import BrawlStars from './BrawlStars'; // Adjust the path if necessary
 import BasePage from './BasePage';
 import { useEffect, useState } from 'react';
 import Characters from './charaters';
+import brawlstarsJson from './assets/brawls.json';
 
-const allCharacters = [
-  {
-      name: "Shelly",
-      image: "https://th.bing.com/th/id/OIP.tMaO1vPm-AiJ_Pm2YAO6mAAAAA?w=115&h=193&c=7&r=0&o=5&pid=1.7"
-  },
-  {
-      name: "Leon",
-      image: "https://th.bing.com/th/id/OIP.P6cnim9x6QykoA4R76UkIwHaFj?w=245&h=184&c=7&r=0&o=5&pid=1.7"
-  },
-  {
-      name: "Nita",
-      image: "https://th.bing.com/th/id/OIP.7UuQK6vtn88CFDzgeDy1ZgHaFj?w=226&h=180&c=7&r=0&o=5&pid=1.7"
-  },
-  {
-      name: "Ruffs",
-      image: "https://th.bing.com/th/id/OIP.N7syvExVZKzJ-TM5w5l97wHaMX?w=115&h=183&c=7&r=0&o=5&pid=1.7"
-  },
-]
+function getLastElement<Type>(array: Type[]): Type | undefined {
+  if (array.length === 0) return undefined;
+  return array[array.length - 1];
+}
+
+const allCharacters = brawlstarsJson.flatMap((category: string[]) => category.map(imageSrc => ({ name: getLastElement(imageSrc.split('/'))?.split('.png')[0], image: imageSrc })));
+
+// const allCharacters = [
+//   {
+//       name: "Shelly",
+//       image: "https://th.bing.com/th/id/OIP.tMaO1vPm-AiJ_Pm2YAO6mAAAAA?w=115&h=193&c=7&r=0&o=5&pid=1.7"
+//   },
+//   {
+//       name: "Leon",
+//       image: "https://th.bing.com/th/id/OIP.P6cnim9x6QykoA4R76UkIwHaFj?w=245&h=184&c=7&r=0&o=5&pid=1.7"
+//   },
+//   {
+//       name: "Nita",
+//       image: "https://th.bing.com/th/id/OIP.7UuQK6vtn88CFDzgeDy1ZgHaFj?w=226&h=180&c=7&r=0&o=5&pid=1.7"
+//   },
+//   {
+//       name: "Ruffs",
+//       image: "https://th.bing.com/th/id/OIP.N7syvExVZKzJ-TM5w5l97wHaMX?w=115&h=183&c=7&r=0&o=5&pid=1.7"
+//   },
+// ]
 
 function App() {
   const [characters, setCharacters] = useState<any[]>(() => {

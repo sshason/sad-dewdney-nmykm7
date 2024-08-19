59a65892409cde9704ce52377baf1d03ba068677
Shahar Sonino <shaharso@microsoft.com>
2024-08-19 21:36:36 +0300

    add new menu items

--- src/menu.tsx
+++ src/menu.tsx
@@ -9,8 +9,9 @@ import { Link } from 'react-router-dom';
 
 const menuItems = [
   { name: 'גלגל שמות', image: WheelFamilyNames, url: 'https://wheelofnames.com/he/n7s-4x8' },
-  { name: 'צבעים באנגלית', image: WheelColorImage, url: 'https://wheelofnames.com/he/822-us7' },
-  { name: 'חלקי גוף', image: WheelBodyPartsImage, url: 'https://wheelofnames.com/he/5f6-3pn' },
+  { name: 'צבעים באנגלית', image: WheelColorImage, url: 'https://wheelofnames.com/he/syn-h4n' },
+  { name: 'חלקי גוף', image: WheelBodyPartsImage, url: 'https://wheelofnames.com/he/tub-vq2' },
+  { name: 'Animals', image: WheelBodyPartsImage, url: 'https://wheelofnames.com/he/wzd-amn' },
   { name: 'פקמן', image: 'https://th.bing.com/th/id/OIP.WnQgGM1kKQDCVouw8iO5kQHaEc?w=300&h=180&c=7&r=0&o=5&pid=1.7', url: 'https://pacman.js.org/' },
   { name: 'חשבון', image: 'https://th.bing.com/th/id/OIP.YQt1G5Ho2Gv6jzCcktHDpwHaHa?w=165&h=180&c=7&r=0&o=5&pid=1.7', url: 'https://www.matific.com/students/app/7.5.7/lite/' },
   { name: 'Brawl Stars', image: 'https://th.bing.com/th/id/OIP.jts_eWK_m0N25cf5oATDFAHaEK?w=321&h=180&c=7&r=0&o=5&pid=1.7', url: '/brawlstars' },

0bfbc209f1559807728190b5050b9a40cc4a6df1
Shahar Sonino <shaharso@microsoft.com>
2024-08-19 21:40:48 +0300

    change animals menu item picture

--- src/menu.tsx
+++ src/menu.tsx
@@ -4,6 +4,7 @@ import WheelBodyPartsImage from './assets/wheel_body_parts.jpg';
 import WheelColorImage from './assets/wheel_colors.png';
 import WheelFamilyNames from './assets/wheel_family_names.png';
 import BrawlStarsOlympics from './assets/brawlstars_olympics.jpg';
+import FrogImage from './assets/frog.webp';
 import { Link } from 'react-router-dom';
 
 
@@ -11,7 +12,7 @@ const menuItems = [
   { name: 'גלגל שמות', image: WheelFamilyNames, url: 'https://wheelofnames.com/he/n7s-4x8' },
   { name: 'צבעים באנגלית', image: WheelColorImage, url: 'https://wheelofnames.com/he/syn-h4n' },
   { name: 'חלקי גוף', image: WheelBodyPartsImage, url: 'https://wheelofnames.com/he/tub-vq2' },
-  { name: 'Animals', image: WheelBodyPartsImage, url: 'https://wheelofnames.com/he/wzd-amn' },
+  { name: 'Animals', image: FrogImage, url: 'https://wheelofnames.com/he/wzd-amn' },
   { name: 'פקמן', image: 'https://th.bing.com/th/id/OIP.WnQgGM1kKQDCVouw8iO5kQHaEc?w=300&h=180&c=7&r=0&o=5&pid=1.7', url: 'https://pacman.js.org/' },
   { name: 'חשבון', image: 'https://th.bing.com/th/id/OIP.YQt1G5Ho2Gv6jzCcktHDpwHaHa?w=165&h=180&c=7&r=0&o=5&pid=1.7', url: 'https://www.matific.com/students/app/7.5.7/lite/' },
   { name: 'Brawl Stars', image: 'https://th.bing.com/th/id/OIP.jts_eWK_m0N25cf5oATDFAHaEK?w=321&h=180&c=7&r=0&o=5&pid=1.7', url: '/brawlstars' },
